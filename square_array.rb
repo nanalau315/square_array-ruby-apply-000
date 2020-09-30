a209145b80df181117edd1436c0197b99d8ed4fb
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-30 20:54:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
   # your code here
+  
+  
+  counter = 0
+  
+  array.each do |numbers|
+    
+  array[counter] = numbers ** 2
+  
+  counter += 1
+  
+  end
+  
 end
+
+
+
+#
